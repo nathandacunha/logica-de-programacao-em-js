d91e7367352038df1066f1715e234209778d77b9
nathandacunha <nathan_cunha@estudante.sesisenai.org.br>
2025-12-26 19:42:19 -0300

    feat(DOM): Criação de referência ao formulario, criacao de referência ao h1 e criação evento ao botao ser clicado

--- integracao-com-html/script.js
+++ integracao-com-html/script.js
@@ -1,3 +1,11 @@
-const resp = document.querySelector('h1')
-const cor = document.querySelector('#inCor');
-const lista = document.querySelector('.lista')
+// cria a referência ao form e ao um elemento h1(onde será exibida a resposta)
+
+const formulario = document.querySelector('form');
+const resposta = document.querySelector('h1');
+
+// criar um "ouvinte" de evento, acionado quando o botão submit for clicado
+formulario.addEventListener("submit", (e) => {
+    const nome = formulario.inNome.value;
+    resposta.innerText = `Olá ${nome}`;
+})
+
