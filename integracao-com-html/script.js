1ccb0a174b5a7b1881427e7eb400f25650d773b6
nathandacunha <nathan_cunha@estudante.sesisenai.org.br>
2026-01-28 16:05:36 -0300

    add(DOM): adicionado método preventDefault

--- integracao-com-html/script.js
+++ integracao-com-html/script.js
@@ -6,6 +6,6 @@ const resposta = document.querySelector('h1');
 // criar um "ouvinte" de evento, acionado quando o botão submit for clicado
 formulario.addEventListener("submit", (e) => {
     const nome = formulario.inNome.value;
-    resposta.innerText = `Olá ${nome}`;
-})
-
+    resposta.innerText = `Ola ${nome}`;
+    e.preventDefault();
+});
